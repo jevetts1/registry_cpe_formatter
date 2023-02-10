8b82895e80f77f7bfa9ed48537f8bcbe658f4606
jevetts1 <106483754+jevetts1@users.noreply.github.com>
2023-02-10 16:55:12 +0000

    Now queries 32bit
    registry_query.py now also queries 32bit software from the registry.

--- registry_query.py
+++ registry_query.py
@@ -1,30 +1,35 @@
 import winreg
 
 def get_installed_software(): #returns a list of dictionaries of {Vendor:vendor,Name:name,Version:version}
+    _32bit_software_key = r"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall" 
+    _64bit_software_key = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+
     software_list = []
-    key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall") #software information is easy to access in the Uninstall registry key
 
-    for i in range(winreg.QueryInfoKey(key)[0]): #iterates through all subkeys (software) then appends a dictionary to the list with the information inside
-        software = {}
+    for software_key in [_32bit_software_key,_64bit_software_key]:
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,software_key) #software information is easy to access in the Uninstall registry key
 
-        subkey = winreg.EnumKey(key, i)
-        subkey_path = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" + "\\" + subkey
-        subkey_handle = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, subkey_path)
-        
-        try:
-            software["Vendor"] = winreg.QueryValueEx(subkey_handle, "Publisher")[0]
-        except:
-            pass
-        try:
-            software["Name"] = winreg.QueryValueEx(subkey_handle, "DisplayName")[0]
-        except:
-            pass
-        try:
-            software["Version"] = winreg.QueryValueEx(subkey_handle, "DisplayVersion")[0]
-        except:
-            pass
+        for i in range(winreg.QueryInfoKey(key)[0]): #iterates through all subkeys (software) then appends a dictionary to the list with the information inside
+            software = {}
 
-        software_list.append(software)
+            subkey = winreg.EnumKey(key, i)
+            subkey_path = software_key + "\\" + subkey
+            subkey_handle = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,subkey_path)
+            
+            try:
+                software["Vendor"] = winreg.QueryValueEx(subkey_handle, "Publisher")[0]
+            except:
+                pass
+            try:
+                software["Name"] = winreg.QueryValueEx(subkey_handle, "DisplayName")[0]
+            except:
+                pass
+            try:
+                software["Version"] = winreg.QueryValueEx(subkey_handle, "DisplayVersion")[0]
+            except:
+                pass
+
+            software_list.append(software)
 
     return software_list
 
